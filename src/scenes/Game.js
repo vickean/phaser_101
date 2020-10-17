7d59c21a4b49f74a442e8f99bf9565f624e65379
vickean <vickean@gmail.com>
2020-10-17 19:54:08 +0800

    Update structure to use parcel-bundler instead of http-server as we want to use ES6

---
+++ src/scenes/Game.js
@@ -0,0 +1,11 @@
+import Phaser from "phaser";
+
+export default class Game extends Phaser.Scene {
+  preload() {}
+
+  create() {
+    const ball = this.add.circle(400, 250, 10, 0xffffff, 1);
+    this.physics.add.existing(ball);
+    ball.body.setVelocity(200, 200);
+  }
+}
